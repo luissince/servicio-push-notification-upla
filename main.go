5bc28f2d4fb6881a0d5a692129e043a136b43b79
Informatica <llara@mail.upla.edu.pe>
2023-04-10 11:06:12 -0500

    Nuevos cambios de las variables de entorno

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	// "fmt"
+	"os"
 	"servicio-pushnotificacion/src/controller"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	time.LoadLocation("America/Lima")
-	godotenv.Load();
+	godotenv.Load()
 
-	var go_port string = os.Getenv("GO_PORT");
+	var go_port string = os.Getenv("GO_PORT")
 
 	router := gin.Default()
 
